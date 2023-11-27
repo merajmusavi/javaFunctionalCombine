0c091d19cc8a4707f0279a387a91b515799d4c38
merajuni7ed <merajmusavi1243m@gmail.com>
2023-11-27 23:31:06 +0330

    writing the combination in main file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Player player = new Player(17,102,"meraj");
+
+        PlayerValidator playerValidator = PlayerValidator.validate_age();
+        PlayerValidator combined = playerValidator.and(PlayerValidator.validate_goals());
+
+
+        ValidateCondition apply = combined.apply(player);
+        System.out.println(apply);
+
     }
 }
